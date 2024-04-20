6d35f58ec2ae54592b345edc4f86c99151ee67f9
James Browning <JamesB.fe80@gmail.com>
2024-04-20 03:28:54 -0700

    Convert statustoa to have more Python and less C

--- pylib/ntpc.py
+++ pylib/ntpc.py
@@ -13,6 +13,8 @@ import os.path
 import re
 import sys
 import time
+import ntp.control
+import ntp.magic
 import ntp.poly
 
 LIB = 'ntpc'
@@ -110,12 +112,6 @@ def _lfp_wrap(callback, in_string):
     return out_value
 
 
-def statustoa(i_type, i_st):
-    """Convert a time stamp to something readable."""
-    mid_str = _statustoa(i_type, i_st)
-    return ntp.poly.polystr(mid_str)
-
-
 def msyslog(level, in_string):
     """Log send a message to terminal or output."""
     mid_bytes = ntp.poly.polybytes(in_string)
@@ -192,3 +188,156 @@ def lfptofloat(in_string):
     l_fp = ihextolfp(in_string[2:])
     tspec = lfp_stamp_to_tspec(l_fp)
     return tspec[0] + (tspec[1] / 1e9)
+
+
+# --- === *** === ---
+
+
+clock_codes = [
+    [ntp.control.CTL_CLK_OKAY,         "clk_unspec"],
+    [ntp.control.CTL_CLK_NOREPLY,      "clk_no_reply"],
+    [ntp.control.CTL_CLK_BADFORMAT,    "clk_bad_format"],
+    [ntp.control.CTL_CLK_FAULT,        "clk_fault"],
+    [ntp.control.CTL_CLK_PROPAGATION,  "clk_bad_signal"],
+    [ntp.control.CTL_CLK_BADDATE,      "clk_bad_date"],
+    [ntp.control.CTL_CLK_BADTIME,      "clk_bad_time"],
+    [-1,                               "clk"],
+]
+leap_codes = [
+    [ntp.magic.LEAP_NOWARNING,       "leap_none"],
+    [ntp.magic.LEAP_ADDSECOND,       "leap_add_sec"],
+    [ntp.magic.LEAP_DELSECOND,       "leap_del_sec"],
+    [ntp.magic.LEAP_NOTINSYNC,       "leap_alarm"],
+    [-1,                             "leap"],
+]
+sync_codes = [
+    [ntp.control.CTL_SST_TS_UNSPEC,    "sync_unspec"],
+    [ntp.control.CTL_SST_TS_ATOM,      "sync_pps"],
+    [ntp.control.CTL_SST_TS_LF,        "sync_lf_radio"],
+    [ntp.control.CTL_SST_TS_HF,        "sync_hf_radio"],
+    [ntp.control.CTL_SST_TS_UHF,       "sync_uhf_radio"],
+    [ntp.control.CTL_SST_TS_LOCAL,     "sync_local"],
+    [ntp.control.CTL_SST_TS_NTP,       "sync_ntp"],
+    [ntp.control.CTL_SST_TS_UDPTIME,   "sync_other"],
+    [ntp.control.CTL_SST_TS_WRSTWTCH,  "sync_wristwatch"],
+    [ntp.control.CTL_SST_TS_TELEPHONE, "sync_telephone"],
+    [-1,                               "sync"],
+]
+sys_codes = [
+    [ntp.magic.EVNT_UNSPEC,          "unspecified"],
+    [ntp.magic.EVNT_NSET,            "freq_not_set"],
+    [ntp.magic.EVNT_FSET,            "freq_set"],
+    [ntp.magic.EVNT_SPIK,            "spike_detect"],
+    [ntp.magic.EVNT_FREQ,            "freq_mode"],
+    [ntp.magic.EVNT_SYNC,            "clock_sync"],
+    [ntp.magic.EVNT_SYSRESTART,      "restart"],
+    [ntp.magic.EVNT_SYSFAULT,        "panic_stop"],
+    [ntp.magic.EVNT_NOPEER,          "no_sys_peer"],
+    [ntp.magic.EVNT_ARMED,           "leap_armed"],
+    [ntp.magic.EVNT_DISARMED,        "leap_disarmed"],
+    [ntp.magic.EVNT_LEAP,            "leap_event"],
+    [ntp.magic.EVNT_CLOCKRESET,      "clock_step"],
+    [ntp.magic.EVNT_KERN,            "kern"],
+    [ntp.magic.EVNT_TAI,             "TAI"],
+    [ntp.magic.EVNT_LEAPVAL,         "stale_leapsecond_values"],
+    [-1,                             "evnt"],
+]
+select_codes = [
+    [ntp.control.CTL_PST_SEL_REJECT,   "sel_reject"],
+    [ntp.control.CTL_PST_SEL_SANE,     "sel_falsetick"],
+    [ntp.control.CTL_PST_SEL_CORRECT,  "sel_excess"],
+    [ntp.control.CTL_PST_SEL_SELCAND,  "sel_outlier"],
+    [ntp.control.CTL_PST_SEL_SYNCCAND, "sel_candidate"],
+    [ntp.control.CTL_PST_SEL_EXCESS,   "sel_backup"],
+    [ntp.control.CTL_PST_SEL_SYSPEER,  "sel_sys.peer"],
+    [ntp.control.CTL_PST_SEL_PPS,      "sel_pps.peer"],
+    [-1,                               "sel"],
+]
+peer_codes = [
+    [ntp.magic.PEVNT_MOBIL,    "mobilize"],
+    [ntp.magic.PEVNT_DEMOBIL,  "demobilize"],
+    [ntp.magic.PEVNT_UNREACH,  "unreachable"],
+    [ntp.magic.PEVNT_REACH,    "reachable"],
+    [ntp.magic.PEVNT_RESTART,  "restart"],
+    [ntp.magic.PEVNT_REPLY,    "no_reply"],
+    [ntp.magic.PEVNT_RATE,     "rate_exceeded"],
+    [ntp.magic.PEVNT_DENY,     "access_denied"],
+    [ntp.magic.PEVNT_ARMED,    "leap_armed"],
+    [ntp.magic.PEVNT_NEWPEER,  "sys_peer"],
+    [ntp.magic.PEVNT_CLOCK,    "clock_event"],
+    [ntp.magic.PEVNT_AUTH,     "bad_auth"],
+    [ntp.magic.PEVNT_POPCORN,  "popcorn"],
+    [-1,                       "pevnt"],
+]
+peer_st_bits = [
+    [ntp.control.CTL_PST_CONFIG,               "conf"],
+    [ntp.control.CTL_PST_AUTHENABLE,           "authenb"],
+    [ntp.control.CTL_PST_AUTHENTIC,            "auth"],
+    [ntp.control.CTL_PST_REACH,                "reach"],
+    [ntp.control.CTL_PST_BCAST,                "bcast"],
+]
+
+
+def getcode(tab, key):
+    try:
+        tab2 = [x[0] for x in tab]
+        key2 = tab2.index(key)
+        return tab[key2][1]
+    except ValueError:
+        return tab[-1][1] + '_' + str(key)
+
+
+def sys_status(st):
+    return [
+        getcode(leap_codes, (st >> 14) & 0x3),
+        getcode(sync_codes, (st >> 8) & 0x3f),
+        getevents((st >> 4) & 0xf),
+        getcode(sys_codes, st & 0xf),
+    ]
+
+
+def peer_status(st):
+    pst = 0xff & (st >> 8)
+    ret = [
+        decode_bitflags(pst, ", ", peer_st_bits),
+        getcode(select_codes, pst & 0x7),
+        getevents((st >> 4) & 0xf),
+    ]
+    if (st & ~ntp.magic.PEER_EVENT) != ntp.magic.EVNT_UNSPEC:
+        ret.append(getcode(peer_codes, 128 + (st & 0xf)))  # FIXME
+    return ret
+
+
+def clock_status(st):
+    return [
+        getevents((st >> 4) & 0xf),
+        getcode(clock_codes, st & 0xf),
+    ]
+
+
+def statustoa(typeof, st):
+    return ', '.join(typical[typeof](st)) if typeof in typical else ""
+
+
+def getevents(cnt):
+    """getevents - return a descriptive string for the event count."""
+    if cnt == 0:
+        return "no events"
+    if cnt == 1:
+        return "1 event"
+    return str(cnt) + " events"
+
+
+def decode_bitflags(bits, sep2, tab):
+    pieces = []
+    for row in tab:
+        if row[0] & bits:
+            pieces.append(row[1])
+    return sep2.join(pieces)
+
+
+typical = {
+    ntp.control.TYPE_CLOCK: clock_status,
+    ntp.control.TYPE_PEER: peer_status,
+    ntp.control.TYPE_SYS: sys_status,
+}
