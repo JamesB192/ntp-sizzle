4211c7facf12e52f338159dece765bea61103d8b
James Browning <JamesB.fe80@gmail.com>
2024-04-20 16:40:53 -0700

    Deep cuts in libntp/

--- libntp/ntp_c.c
+++ libntp/ntp_c.c
@@ -10,24 +10,12 @@
  */
 #include "config.h"
 
-#include "ntp_machine.h"
-#include "ntpd.h"
-#include "ntp_io.h"
-#include "ntp_fp.h"
-#include "ntp_stdlib.h"
-#include "ntp_syslog.h"
-#include "timespecops.h"
-
-#include "ntp_config.h"
-#include "ntp_assert.h"
-
+#include "ntp.h"
 #include "ntp_control.h"
 
-#include "pymodule-mac.h"
-
-void ntpc_setprogname(char*);
-bool ntpc_adj_systime(double);
-bool ntpc_step_systime(double);
+int dumbslew(int64_t s, int32_t us);
+int dumbstep(int64_t s, int32_t ns);
+time64_t ntpcal_ntp_to_time(uint32_t ntp, time_t pivot);
 
 /* Don't include anything from OpenSSL */
 
@@ -41,22 +29,6 @@ int CLOCK_TYPE = TYPE_CLOCK;
  * Client utility functions
  */
 
-void
-ntpc_setprogname(char *s)
-{
-	/*
-	 * This function is only called from clients.  Therefore
-	 * log to stderr rather than syslog, and suppress logfile
-	 * impediments.  If we ever want finer-grained control, that
-	 * will be easily implemented with additional arguments.
-	 */
-	syslogit = false;	/* don't log messages to syslog */
-	termlogit = true;	/* duplicate to stdout/err */
-	termlogit_pid = false;
-	msyslog_include_timestamp = false;
-	progname = strdup(s);
-}
-
 int dumbslew(int64_t s, int32_t us) {
     struct timeval step = {s, us};
     return adjtime(&step, NULL);
@@ -65,4 +37,26 @@ int dumbslew(int64_t s, int32_t us) {
 int dumbstep(int64_t s, int32_t ns) {
     struct timespec step = {s, ns};
     return clock_settime(CLOCK_REALTIME, &step);
+}
+
+/* Convert a timestamp in NTP scale to a 64bit seconds value in the UN*X
+ * scale with proper epoch unfolding around a given pivot or the current
+ * system time. This function happily accepts negative pivot values as
+ * timestamps before 1970-01-01, so be aware of possible trouble on
+ * platforms with 32bit 'time_t'!
+ *
+ * This is also a periodic extension, but since the cycle is 2^32 and
+ * the shift is 2^31, we can do some *very* fast math without explicit
+ * divisions.
+ */
+time64_t ntpcal_ntp_to_time(uint32_t ntp, time_t pivot) {
+    time64_t res;
+
+    settime64s(res, pivot);
+    settime64u(res, time64u(res)-0x80000000);	 // unshift of half range
+    ntp	-= (uint32_t)2208988800;		 // warp into UN*X domain
+    ntp	-= time64lo(res);			 // cycle difference
+    settime64u(res, time64u(res)+(uint64_t)ntp); // get expanded time
+
+    return res;
 }
